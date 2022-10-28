8809e6f165a945fc3f3f5996f3922e1719da651b
NahuelPardo89 <nahuel.pardo74@gmail.com>
2022-10-28 12:36:08 -0300

    Por ahora solo probando funciones de modelo

---
+++ Backend/Controlador/controlador.py
@@ -0,0 +1,26 @@
+from sys import path
+
+path.append('C:\\Users\\nanit\\Desktop\\proyecto\\Proyecto-FullStack\\Backend')
+
+
+from modelo.conexionDb import Conexion
+from modelo.modelProducto import ModelProducto
+from modelo.modelUser import ModelUser
+from modelo.clases.users import User,Empleado,Cliente
+from modelo.clases.proveedor import Proveedor
+from modelo.clases.producto import Producto
+from modelo.clases.instalaciones import Instalacion
+
+
+db=Conexion()
+conn=db.getConn()
+instalacion=Instalacion(1,"gym",20)
+user1= User(3,"ricardo","montaner","123456","estrada 1433","admin123")
+empleado= Empleado(user1.getId(),user1.getNombre(),user1.getApellido(),user1.getTel(),user1.getDireccion(),user1.getContraseña(),23,instalacion,"horario")
+provedor= Proveedor(1,"mario","tate","123456",empleado)
+user2=User(3,"pablo","montaner","123456","maipo 100","admin123")
+cliente =Cliente(1,user2.getNombre(),user2.getApellido(),user2.getTel(),user2.getDireccion(),user2.getContraseña(),1,1)
+ModelUser.addCliente(conn,cliente)
+#ModelUser.addUser(conn,user1)
+#ModelUser.addEmpleado(conn,empleado)
+#print (empleado.getId(),empleado.getIdEmpleado(), empleado.getIdInstalacion(),empleado.getHorario())
