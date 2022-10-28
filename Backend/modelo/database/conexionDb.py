3527c6416b5a6f96053fdc963042afffc48033b7
NahuelPardo89 <nahuel.pardo74@gmail.com>
2022-10-28 19:38:51 -0300

    archivo reubicado

--- Backend/modelo/database/conexionDb.py
+++ Backend/modelo/database/conexionDb.py
@@ -5,7 +5,7 @@ class Conexion:
     def __init__(self):
         self.connection=""
         try:
-            connection=mysql.connector.connect(host='localhost',database='ampav1',user='root',password='copado34414604')
+            connection=mysql.connector.connect(host='localhost',database='ampaDB',user='root',password='copado34414604')
             if connection.is_connected():
                     db_Info=connection.get_server_info()
                     print("Connected to MySQL Server version", db_Info)
