48dc1c44bbee76ac0a61ef52a9b66d38743bffca
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2022-10-29 19:51:27 -0300

    Inserto Instalaciones en BD
    -Se insertan las instalaciones en la base de datos

--- Backend/modelo/modelInstalacion.py
+++ Backend/modelo/modelInstalacion.py
@@ -1,3 +1,11 @@
 
 class ModelInstalacion:
-    pass
+    def __init__(self, nombreInstalacion, precio):
+        self.nombreInstalacion = nombreInstalacion
+        self.precio = precio
+
+def instalaciones():
+    canchafutbol= ModelInstalacion(
+        #conexion base de datos 
+        # conexionbd = 
+    )
