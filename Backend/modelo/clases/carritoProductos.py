93e990a40e346ea21f8825d365b2e6ba32f4eead
Catriel <cartup90@gmail.com>
2022-10-30 19:27:39 -0300

    Clase CarritoProductos

---
+++ Backend/modelo/clases/carritoProductos.py
@@ -0,0 +1,11 @@
+class CarritoProductos:
+    def __init__(self, id):
+        self._id = id
+   
+    #Getter
+    def getIdCarrito(self):
+        return self._id
+   
+    #Setter
+    def setIdCarrito(self,id):
+        self._id = id
