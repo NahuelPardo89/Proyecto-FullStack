68caed72d3ced5e0f0a8823db257ec255b026988
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2022-10-29 21:07:04 -0300

    Modelo instalaciones
    Funciones delete, add, udpate para el Modelo instalaciones

--- Backend/modelo/modelInstalacion.py
+++ Backend/modelo/modelInstalacion.py
@@ -1,11 +1,34 @@
+from clases.instalaciones import Instalacion
 
 class ModelInstalacion:
-    def __init__(self, nombreInstalacion, precio):
-        self.nombreInstalacion = nombreInstalacion
-        self.precio = precio
+    @classmethod
+    def addInstalacion(self,conn, instalacion):
+            try:
+                cursor=conn.cursor()
+                cursor.execute("INSERT INTO `instalaciones` ( `idInstalacion`, `nombre`, `precio`)  VALUES (%s,%s,%s)",(
+                instalacion.getIdInstalacion(),instalacion.getnombre(),instalacion.getprecio()))
+                conn.commit()
+            except Exception as ex:
+                raise Exception(ex)
 
-def instalaciones():
-    canchafutbol= ModelInstalacion(
-        #conexion base de datos 
-        # conexionbd = 
-    )
+    def updateInstalacion(self,conn,instalacion):
+        try:
+            cursor=conn.cursor()
+            cursor.execute("""
+                    UPDATE instalaciones
+                    SET idInstalacion=%s,
+                        nombre=%s,
+                        precio=%s,
+                        
+                WHERE idInstalacion=%s
+                """,(instalacion.getIdInstalacion(),instalacion.getnombre(),instalacion.getPrecio()))
+            conn.commit()
+        except Exception as ex:
+            raise Exception(ex)
+    def deleteInstalacion(self,conn,instalacion):
+        try:
+            cursor=conn.cursor()
+            cursor.execute("DELETE FROM instalaciones WHERE idInstalacion=%s",(instalacion.getId()))
+            conn.commit()
+        except Exception as ex:
+            raise Exception(ex)
