72fd54453218056f9f7c6fda384d5b82ec67be6e
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2022-10-31 20:33:05 -0300

    Class Instalaciones: Setters y Getters
    Se definen los  Setters y Getters para la clase de INSTALACIONES

--- Backend/modelo/clases/instalaciones.py
+++ Backend/modelo/clases/instalaciones.py
@@ -1,12 +1,29 @@
 class Instalacion():
     def __init__(self, id,nombre,precio):
         
-        self._id= id
+        self._idInstalacion= id
         self._nombre= nombre
         self._precio= precio
         
-    #Getters
+    
+#Getters
 
     def getidInstalacion(self):
-        return self._id
+        return self._idInstalacion
+    def getnombre(self):
+        return self._nombre
+    def getprecio(self):
+        return self._precio
+
+#Setters
+    def setidInstalacion(self, id):
+        self._idInstalacion = id
+    def setnombre(self, nombre):
+        self._nombre = nombre    
+    def setprecio(self, precio):
+        self._precio = precio
+
+
+
+    
         
