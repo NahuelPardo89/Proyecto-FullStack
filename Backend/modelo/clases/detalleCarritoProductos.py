9242487efee87822e782089417faf48f75b40b2a
SOFIA <ledesmasofiagimena@hotmail.com>
2022-10-30 22:13:58 -0300

    detallecarritoproducto

---
+++ Backend/modelo/clases/detalleCarritoProductos.py
@@ -0,0 +1,7 @@
+class DetalleCarritoProductos():
+    def __init__(self, id,idCarritoProductos,idProducto,cantidad):
+        
+        self._id=id
+        self._idCarritoProductos= idCarritoProductos
+        self._idProducto=idProducto
+        self._cantidad= cantidad
