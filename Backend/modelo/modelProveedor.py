8386a1fcadee5700d88dce5590e0e9aad5281875
NahuelPardo89 <106750741+NahuelPardo89@users.noreply.github.com>
2022-11-07 20:32:55 -0300

    metodo add implementado

--- Backend/modelo/modelProveedor.py
+++ Backend/modelo/modelProveedor.py
@@ -1,2 +1,10 @@
 class ModelProveedor:
-    pass
+    @classmethod
+    def addProveedor(self,conn,proveedor):
+        try:
+            cursor=conn.cursor()
+            cursor.execute("INSERT INTO `proveedores`(`idProveedor`,`idEmpleado`,`Nombre`,`empresa`,`telefono`) VALUES (%s,%s,%s,%s,%s)",(
+            proveedor.getId(),proveedor.getIdEmpleado(),proveedor.getNombre(),proveedor.getEmpresa(),proveedor.getTelefono()))
+            conn.commit()
+        except Exception as ex:
+            raise Exception(ex)
