c747f4bdc48771ebc7ee479b8c756b47ac2cc05b
NahuelPardo89 <nahuel.pardo74@gmail.com>
2022-11-14 08:47:56 -0300

    cache

--- Backend/modelo/database/initDataBase.py
+++ Backend/modelo/database/initDataBase.py
@@ -1,9 +1,11 @@
 from sys import path
 #PARA IMPORTAR DESDE MODELO
-path.append('C:\\Users\\nanit\\OneDrive\\Escritorio\\Proyecto\\Proyecto-FullStack')
+
+path.append('C:/Users/nanit/Desktop/proyecto/Proyecto-FullStack')
 
 
-
+#bd
+from Backend.modelo.database.conexionDb import Conexion
 
 #MODELOS
 from Backend.modelo.modelProducto import ModelProducto
@@ -43,7 +45,7 @@ class InitDb():
             #########################################  TABLA EMPELADOS  ########################################################## ##############                                                           
                             #   DNI     | NOMBRE  | APELLIDO  |TELEFONO   |DIRECCION       CONTRASEÑA| IDEMPLEADO | INSTALACION  |HORARIO
             empleado1=Empleado(25492666, "Juana",   "funes",    "351461325","Maipu 166",    "JuanaFu25",      1,         gimnasio,   "8:00-14:00")
-            empleado2=Empleado(25492555, "Pedro",   "Gonzalez", "351444333","Estrada 166",  "Hola123456",     2,         paddle,     "8:00-14:00")
+            empleado2=Empleado(25492555, "Pedro",   "Gonzalez", "351444333","Estrada 166",  "Hola123456",     7,         paddle,     "8:00-14:00")
             empleado3=Empleado(40111222, "David",   "Gimenez",  "351444666","9 de Julio 25","David321",       3,         futbol,     "14:00-22:00")
             empleado4=Empleado(14555621, "Vilma",   "Stefanini","351444666","San Juan 1122","Vilma28",        4,         futbol,     "14:00-22:00")
             
@@ -63,16 +65,16 @@ class InitDb():
             
                                   #ID| ID PROVEEDOR   | NOMBRE           | MARCA       |DESCRIPCION        |PRECIO  |STOCK   | FOTO
             cantimplora = Producto( 1 ,     1,        "BotellaTérmica"  ,"Everlast"   ,"1 ltrs frio/calor"  ,2584     ,50,    "botella-everlast-entrenamiento.jpg" )
-            zapatilla   = Producto( 2 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 42"           ,35899    , 5,    "nike-airmax-correlate.jpg")
-            zapatilla   = Producto( 3 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 41"           ,35899    , 5,    "nike-airmax-correlate.jpg")
-            zapatilla   = Producto( 4 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 40"           ,35899    , 5,    "nike-airmax-correlate.jpg")
-            zapatilla   = Producto( 5 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 39"           ,35899    , 2,    "nike-airmax-correlate.jpg")
+            zapatilla1   = Producto( 2 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 42"           ,35899    , 5,    "nike-airmax-correlate.jpg")
+            zapatilla2   = Producto( 3 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 41"           ,35899    , 5,    "nike-airmax-correlate.jpg")
+            zapatilla3   = Producto( 4 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 40"           ,35899    , 5,    "nike-airmax-correlate.jpg")
+            zapatilla4   = Producto( 5 ,     2,        "Zapatilla"       ,"NikeAirMax" ,"talle 39"           ,35899    , 2,    "nike-airmax-correlate.jpg")
             kitfitness  = Producto( 6 ,     1,        "KitFitnessHome"  ,"Everlast"   ,"kit 3 piezas"       ,9215     ,15,    "Kit-fitness.jpg")
             gorrotenis  = Producto( 7 ,     2,        "GorroTenisBlade" ,"Wilson"     ,"Gorro tenis"        ,4799     ,10,    "gorra-tenis-wilson.jpg")
-            shortadidas = Producto( 8 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready S" ,10599    , 5,    "short-adidas.jpg")
-            shortadidas = Producto( 9 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready M" ,10599    , 5,    "short-adidas.jpg")
-            shortadidas = Producto(10 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready L" ,10599    , 5,    "short-adidas.jpg")
-            shortadidas = Producto(11 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready XL",10599    , 5,    "short-adidas.jpg")
+            shortadidas1 = Producto( 8 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready S" ,10599    , 5,    "short-adidas.jpg")
+            shortadidas2 = Producto( 9 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready M" ,10599    , 5,    "short-adidas.jpg")
+            shortadidas3 = Producto(10 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready L" ,10599    , 5,    "short-adidas.jpg")
+            shortadidas4 = Producto(11 ,     2,        "ShortAdidasAero" ,"Adidas"     ,"Short Aero Ready XL",10599    , 5,    "short-adidas.jpg")
             raquetatenis= Producto(12 ,     1,        "RaquetaTenis"    ,"Wilson"     ,"Raqueta Wilson"     ,69999    ,10,    "raqueta-de-tenis-wilson.jpg")
             #########################################################################################################################    
             
@@ -99,10 +101,20 @@ class InitDb():
             
             #INSERT PRODUCTOS
             ModelProducto.addProducto(conn,cantimplora)
-            ModelProducto.addProducto(conn,zapatilla)
+            ModelProducto.addProducto(conn,zapatilla1)
+            ModelProducto.addProducto(conn,zapatilla2)
+            ModelProducto.addProducto(conn,zapatilla3)
+            ModelProducto.addProducto(conn,zapatilla4)
+            ModelProducto.addProducto(conn,kitfitness)
+            ModelProducto.addProducto(conn,gorrotenis)
+            ModelProducto.addProducto(conn,shortadidas1)
+            ModelProducto.addProducto(conn,shortadidas2)
+            ModelProducto.addProducto(conn,shortadidas3)
+            ModelProducto.addProducto(conn,shortadidas4)
+            ModelProducto.addProducto(conn,raquetatenis)
 
-            #devuelvo database
             
-    
+db= Conexion()
+conn= db.connection    
 
-    
+InitDb(conn)
