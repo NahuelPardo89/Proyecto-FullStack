c18d699043f255fb973e4dc48535db9dddf98a5f
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-04-28 11:25:26 -0300

    Modulos: dashboard,productos,reservas inicializado

--- FrontEnd/ampa/src/app/modules/home/home.module.ts
+++ FrontEnd/ampa/src/app/modules/home/home.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ContactoComponent,
+    AboutusComponent
   ],
   imports: [
     CommonModule,
---
+++ FrontEnd/ampa/src/app/modules/reservas/reservas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReservasRoutingModule } from './reservas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReservasRoutingModule
+  ]
+})
+export class ReservasModule { }
