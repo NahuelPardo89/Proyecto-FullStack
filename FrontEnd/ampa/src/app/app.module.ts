010d3a4b9c1356c8fb5126cdeb4ae445b781f57e
NahuelPardo89 <106750741+NahuelPardo89@users.noreply.github.com>
2023-04-28 11:09:58 -0300

    modulo Home y shared Agregado

--- FrontEnd/ampa/src/app/app.module.ts
+++ FrontEnd/ampa/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './modules/home/home.module';
+import { SharedModule } from './modules/shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HomeModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd/ampa/src/app/modules/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    NavComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
