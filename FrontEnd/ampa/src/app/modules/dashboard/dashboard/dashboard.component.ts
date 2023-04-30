a110998be86af51248f6f2ee082d7cf90b106752
Catriel <cartup90@gmail.com>
2023-04-30 18:15:23 -0300

    Dashboard Productos vendidos

--- FrontEnd/ampa/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ FrontEnd/ampa/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -28,6 +28,25 @@ export class DashboardComponent {
       ];
     })
   );
+    
+  
+  //LOS PRODUCTOS VENDIDOS LLEGAN COMO OBJETO AQUI
+  ventas = [
+  {
+    nombre:"Nike Air",
+    categoria: "Zapatillas",
+    img: "../../../../assets/img/nike-airmax-correlate.jpg",
+    precio: "45.000"
+  },
+  {
+    nombre:"Wilson Roland Garros",
+    categoria:"Raquetas",
+    img: "../../../../assets/img/WILSON-Roland-Garros.jpg",
+    precio: "85.000"
+  }
+]
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
+
+
