a15e9fb760ab2129b0d07ab3c994ecc3ef14c535
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-08 09:56:36 -0300

    agregando lazy loading

--- FrontEnd/ampa/src/app/app-routing.module.ts
+++ FrontEnd/ampa/src/app/app-routing.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InstalacionesComponent } from './modules/reservas/components/instalaciones/instalaciones.component';
-import { AgendaComponent } from './modules/reservas/components/agenda/agenda.component';
-import { ContactoComponent } from './modules/home/components/contacto/contacto.component';
-import { ProductosComponent } from './modules/productos/components/productos/productos.component';
-import { CarritoComponent } from './modules/productos/components/carrito/carrito.component';
-import { AboutusComponent } from './modules/home/components/aboutus/aboutus.component';
+
 const routes: Routes = [
   {path: 'home',loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
   {path: 'dashboard',loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path: 'auth',loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
-  {path: 'reservas', component: InstalacionesComponent },
-  {path:'reservas/agenda', component: AgendaComponent },
-  {path: 'contacto', component: ContactoComponent },
-  {path: 'productos', component: ProductosComponent},
-  {path: 'productos/carrito', component: CarritoComponent},
+  
   {path:'**', redirectTo:'/home'},
-  {path:'home/aboutus', component:AboutusComponent}
  ];
 
 @NgModule({
