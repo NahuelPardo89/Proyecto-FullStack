103bfa3ac44e64043955b89f9974a16081e69f0d
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-18 19:31:14 -0300

    modificando ubicacion de fotos

--- BackEnd/ampaApiRest/ecommerce/models.py
+++ BackEnd/ampaApiRest/ecommerce/models.py
@@ -31,7 +31,7 @@ class Producto(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2,null=False, blank=False)
     stock = models.IntegerField(null=False, blank=False)
-    foto = models.ImageField(upload_to='productos/', null=True, blank=True)
+    foto = models.ImageField(upload_to='assets/img/productos/', null=True, blank=True)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.SET_NULL,null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL,null=True, blank=True)
 
