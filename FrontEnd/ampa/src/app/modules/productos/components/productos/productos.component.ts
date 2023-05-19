acdb18a9c0ecc23c4d4005b723654635829f9a81
Tu nombre de usuario <cartup90@gmail.com>
2023-05-19 13:26:50 -0300

    consumo de json de productos

--- FrontEnd/ampa/src/app/modules/productos/components/productos/productos.component.ts
+++ FrontEnd/ampa/src/app/modules/productos/components/productos/productos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from './productos.service';
+
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
 
+  productos: any[];
+
+
+  constructor(private ProductosService: ProductosService) {
+    this.productos = [];
+  }
+  ngOnInit() {
+    this.ProductosService.getProductos()
+      .subscribe(data => {
+        this.productos = data.productos;
+      });
+  }
 }
