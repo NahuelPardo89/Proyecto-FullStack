1b439699909490c64792e900d7598938c7993bc7
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-22 08:58:06 -0300

    registro usuario funcional

---
+++ BackEnd/ampaApiRest/usuarios/views.py
@@ -0,0 +1,9 @@
+
+
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
