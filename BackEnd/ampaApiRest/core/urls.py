5553199e020b8637edc1c8421c6d16f9b8015165
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-31 17:27:44 -0300

    agregando url tienda

--- BackEnd/ampaApiRest/core/urls.py
+++ BackEnd/ampaApiRest/core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
-    # ... rest of your URLs
+    path('tienda/', include('ecommerce.urls')),
+   
 ]
