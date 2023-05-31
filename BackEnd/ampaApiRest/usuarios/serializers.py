a6267243dc347733e04fef652d78a5b2a5693348
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-31 17:03:48 -0300

    usuario, registro y login

--- BackEnd/ampaApiRest/usuarios/serializers.py
+++ BackEnd/ampaApiRest/usuarios/serializers.py
@@ -1,7 +1,40 @@
-from rest_framework import serializers
-from .models import Usuario
 
+from .models import Usuario
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+# User Serializer
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['dni', 'nombre', 'apellido', 'telefono', 'direccion', 'is_active', 'is_staff']
+        fields = ('id', 'dni', 'nombre', 'apellido', 'email', 'telefono', 'direccion')
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('id', 'dni', 'nombre', 'apellido', 'email', 'telefono', 'direccion', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = Usuario.objects.create_user(
+            dni=validated_data['dni'], 
+            password=validated_data['password'],
+            nombre=validated_data['nombre'], 
+            apellido=validated_data['apellido'],
+            email=validated_data['email'], 
+            telefono=validated_data['telefono'], 
+            direccion=validated_data['direccion']
+        )
+        return user
+
+# Login Serializer
+class LoginSerializer(serializers.Serializer):
+    dni = serializers.IntegerField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Usuario o contraseña incorrecto")
