a7f6bbd415888d220870f0143e137c8798cdd424
NahuelPardo89 <nahuel.pardo74@gmail.com>
2023-05-31 17:03:17 -0300

    Custom Usuario y Mananger creados

--- BackEnd/ampaApiRest/usuarios/models.py
+++ BackEnd/ampaApiRest/usuarios/models.py
@@ -2,37 +2,43 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class UsuarioManager(BaseUserManager):
-    def create_user(self, dni, password=None, nombre=None, apellido=None, telefono=None,email=None, direccion=None, is_staff=False, is_superuser=False,**extra_fields):
+    def _create_user(self, dni, password, is_staff, is_superuser, **extra_fields):
         if not dni:
-            raise ValueError('El DNI es requerido')
+            raise ValueError('El DNI es obligatorio.')
 
-        user = self.model(
+        nombre = extra_fields.get('nombre')
+        apellido = extra_fields.get('apellido')
+        email = extra_fields.get('email')
+        telefono = extra_fields.get('telefono')
+        direccion = extra_fields.get('direccion')
+
+        if not nombre:
+            raise ValueError('El nombre es obligatorio.')
+        if not apellido:
+            raise ValueError('El apellido es obligatorio.')
+        if not email:
+            raise ValueError('El email es obligatorio.')
+        
+        
+
+        usuario = self.model(
             dni=dni,
-            nombre=nombre,
-            apellido=apellido,
-            email=email,
-            telefono=telefono,
-            direccion=direccion,
             is_staff=is_staff,
-            is_superuser=is_superuser
+            is_superuser=is_superuser,
+            **extra_fields
         )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        usuario.set_password(password)
+        usuario.save(using=self._db)
+        return usuario
+
+    def create_user(self, dni, password=None, **extra_fields):
+        return self._create_user(dni, password, False, False, **extra_fields)
 
     def create_superuser(self, dni, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superusuario debe tener is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superusuario debe tener is_superuser=True.')
-
-        return self.create_user(dni, password, **extra_fields)
+        return self._create_user(dni, password, True, True, **extra_fields)
 
 class Usuario(AbstractBaseUser, PermissionsMixin):
-    dni = models.IntegerField(max_length=10, unique=True)
+    dni = models.IntegerField( unique=True)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email= models.EmailField()
@@ -40,7 +46,7 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     direccion = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
+    
     objects = UsuarioManager()
 
     USERNAME_FIELD = 'dni'
