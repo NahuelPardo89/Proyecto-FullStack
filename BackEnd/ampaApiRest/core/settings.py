192b3a8368c743c728232c287a087b786f30196b
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2023-06-01 23:01:50 -0500

    Django Api Rest: App Reservas y App instalaciones
    -Se crean las apps de Reservas e instalaciones.
    -Se codifican sus respectivos models

--- BackEnd/ampaApiRest/core/settings.py
+++ BackEnd/ampaApiRest/core/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'reservas',
+    'instalaciones',
 ]
 REST_FRAMEWORK = {   
     'DEFAULT_AUTHENTICATION_CLASSES': (        
