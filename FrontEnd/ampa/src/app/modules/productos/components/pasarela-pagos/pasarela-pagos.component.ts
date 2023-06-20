52c67a27ac7cc5bc6603576dd8b47bdda217162c
Cartup90 <cartup90@gmail.com>
2023-06-19 23:02:17 -0300

    pasarela form reactivo- compra realizada Dialog

--- FrontEnd/ampa/src/app/modules/productos/components/pasarela-pagos/pasarela-pagos.component.ts
+++ FrontEnd/ampa/src/app/modules/productos/components/pasarela-pagos/pasarela-pagos.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Carrito2Service } from '../carrito2/carrito2.service';
 import { PagosService } from '../../services/pagos.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { CompraExitosaComponent } from './compra-exitosa/compra-exitosa.component';
+
 
 
 
@@ -12,8 +16,24 @@ import { PagosService } from '../../services/pagos.service';
 })
 export class PasarelaPagosComponent {
   carrito: any = null;
+  formulario: FormGroup;
 
-  constructor(private carritoService: Carrito2Service,private pagos:PagosService) { }
+  constructor(private carritoService: Carrito2Service, private pagos: PagosService, private formBuilder: FormBuilder, private dialog: MatDialog) {
+    this.formulario = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.pattern(/^\d{7,9}$/)]],
+      nombreTitular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
+      fecha: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]],
+      cvc: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]],
+      pais: ['', Validators.required],
+      provincia: ['', Validators.required],
+      localidad: ['', Validators.required],
+      direccion: ['', Validators.required],
+      terminos: [false, Validators.requiredTrue]
+    });
+  }
 
   ngOnInit(): void {
     const usuarioString = localStorage.getItem('user');
@@ -44,6 +64,23 @@ export class PasarelaPagosComponent {
       }
       
     );
+    if (this.formulario.valid) {
+      // Realiza las acciones necesarias para procesar el pago
+  
+      // Abre el diálogo de compra exitosa
+      const dialogRef = this.dialog.open(CompraExitosaComponent, {
+        width: '80%',
+        disableClose: true, // Impide cerrar el diálogo haciendo clic fuera de él
+      });
+  
+      // Opcional: Realiza acciones adicionales después de que se cierre el diálogo
+      dialogRef.afterClosed().subscribe(result => {
+        // ...
+      });
+    } else {
+      // El formulario no es válido, realiza acciones adicionales o muestra mensajes de error
+    }
+  
     
   }
   
